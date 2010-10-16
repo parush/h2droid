017c56021d86ddca6b4319f0b11c03c5b4b03b5e
fcalise@gmail.com <fcalise@gmail.com@e497a985-b2e5-56b5-e06b-30a1cf0c0d81>
2010-10-16 03:37:44 +0000

    added options menu (via menu inflater) and handle settings menu click

--- src/com/frankcalise/h2droid/h2droid.java
+++ src/com/frankcalise/h2droid/h2droid.java
@@ -1,13 +1,41 @@
 package com.frankcalise.h2droid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class h2droid extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.main);
+    }
+    
+    /** Setup menu for main activity */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	super.onCreateOptionsMenu(menu);
+    	
+    	MenuInflater inflater = getMenuInflater();
+    	inflater.inflate(R.menu.main_menu, menu);
+    	
+    	return true;
+    }
+    
+    /** Handle menu selection */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+    		case R.id.menu_settings:
+    			startActivity(new Intent(this, Settings.class));
+    			return true;
+    	}
+    	
+    	return false;
     }
 }

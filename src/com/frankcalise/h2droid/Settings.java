0993cfe8b7a967b5c6b6d29a98af80f1f72e88d6
fcalise@gmail.com <fcalise@gmail.com@e497a985-b2e5-56b5-e06b-30a1cf0c0d81>
2010-11-04 15:20:32 +0000

    added getAmount getter to retrieve goal from shared prefs (issue 2)

--- src/com/frankcalise/h2droid/Settings.java
+++ src/com/frankcalise/h2droid/Settings.java
@@ -1,12 +1,33 @@
 package com.frankcalise.h2droid;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
 public class Settings extends PreferenceActivity {
+	
+	private static final String OPT_AMOUNT = "SETTING_GOAL";
+	private static final String OPT_AMOUNT_DEF = "64";
+	private static final double DEFAULT_AMOUNT = 64.0;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceBundle) {
 		super.onCreate(savedInstanceBundle);
 		addPreferencesFromResource(R.xml.settings);
+	}
+	
+	/** Get the current value of daily goal amount */
+	public static double getAmount(Context context) {
+		try {
+			String prefString = PreferenceManager.getDefaultSharedPreferences(context)
+				.getString(OPT_AMOUNT, OPT_AMOUNT_DEF);
+			
+			double prefAmount = Double.valueOf(prefString).doubleValue();
+			
+			return prefAmount;
+		} catch (NumberFormatException nfe) {
+			return DEFAULT_AMOUNT;
+		} 
 	}
 }

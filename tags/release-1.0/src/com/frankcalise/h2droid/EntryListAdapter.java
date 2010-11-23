cfb17ba820c4c1e964ca7fbfe23d87727d160d8c
fcalise@gmail.com <fcalise@gmail.com@e497a985-b2e5-56b5-e06b-30a1cf0c0d81>
2010-11-23 02:11:58 +0000

    Release 1.0

---
+++ tags/release-1.0/src/com/frankcalise/h2droid/EntryListAdapter.java
@@ -0,0 +1,68 @@
+package com.frankcalise.h2droid;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class EntryListAdapter extends BaseAdapter {
+	
+	private List<Entry> mEntryList;
+	private final LayoutInflater mInflater;
+	private boolean mIsDetail;
+	
+	public EntryListAdapter(List<Entry> _entryList, Context _context, boolean _isDetail) {
+		this.mEntryList = _entryList;
+		this.mIsDetail = _isDetail;
+		
+		mInflater = LayoutInflater.from(_context);
+	}
+
+	@Override
+	public int getCount() {
+		return mEntryList.size();
+	}
+
+	@Override
+	public Entry getItem(int position) {
+		return mEntryList.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		Entry e = mEntryList.get(position);
+		
+		// Check to see if convertView contains a usable item
+		// If null, inflate a new row item
+		if (convertView == null) {
+			if (mIsDetail == false) {
+				convertView = mInflater.inflate(R.layout.entry_list_item, parent, false);
+			} else {
+				convertView = mInflater.inflate(R.layout.entry_detail_list_item, parent, false);
+			}
+		}
+
+		if (mIsDetail == false) {
+			// Set date
+			((TextView)convertView.findViewById(R.id.entry_date_textview)).setText(e.getDateWithFormat("EEEE, MMMM dd, yyyy"));
+		} else {
+			// Set date
+			((TextView)convertView.findViewById(R.id.entry_date_textview)).setText(e.getDateWithFormat("hh:m:s a"));
+			
+			// Set amount
+			((TextView)convertView.findViewById(R.id.entry_amount_textview)).setText(String.format("  %.1f fl oz", e.getNonMetricAmount()));
+		}
+		
+		return convertView;
+	}
+
+}

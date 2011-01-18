e38f9eda8d2843eb9160d8008bdd813ac960a183
fcalise@gmail.com <fcalise@gmail.com@e497a985-b2e5-56b5-e06b-30a1cf0c0d81>
2011-01-18 13:30:53 +0000

    added display of current favorite amounts to the summary of the preferences (issue 14)

--- src/com/frankcalise/h2droid/Settings.java
+++ src/com/frankcalise/h2droid/Settings.java
@@ -1,11 +1,14 @@
 package com.frankcalise.h2droid;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 
-public class Settings extends PreferenceActivity {
+public class Settings extends PreferenceActivity implements OnSharedPreferenceChangeListener {
 	
 	private static final String OPT_AMOUNT = "SETTING_GOAL";
 	private static final String OPT_AMOUNT_DEF = "64";
@@ -18,10 +21,48 @@ public class Settings extends PreferenceActivity {
 													"FAV_AMOUNT_FOUR",
 													"FAV_AMOUNT_FIVE"};
 	
+	private EditTextPreference mEditTextPref1;
+	private EditTextPreference mEditTextPref2;
+	private EditTextPreference mEditTextPref3;
+	private EditTextPreference mEditTextPref4;
+	private EditTextPreference mEditTextPref5;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceBundle) {
 		super.onCreate(savedInstanceBundle);
+		
+		// Load XML prefs file
 		addPreferencesFromResource(R.xml.settings);
+		
+		// Get reference to preferences to update summaries
+		mEditTextPref1 = (EditTextPreference)getPreferenceScreen().findPreference(OPT_FAV_AMOUNT[0]);
+		mEditTextPref2 = (EditTextPreference)getPreferenceScreen().findPreference(OPT_FAV_AMOUNT[1]);
+		mEditTextPref3 = (EditTextPreference)getPreferenceScreen().findPreference(OPT_FAV_AMOUNT[2]);
+		mEditTextPref4 = (EditTextPreference)getPreferenceScreen().findPreference(OPT_FAV_AMOUNT[3]);
+		mEditTextPref5 = (EditTextPreference)getPreferenceScreen().findPreference(OPT_FAV_AMOUNT[4]);
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		
+		// Setup initial values
+		mEditTextPref1.setSummary("Current amount is " + getPreferenceScreen().getSharedPreferences().getString(OPT_FAV_AMOUNT[0], OPT_FAV_AMOUNT_DEF[0]));
+		mEditTextPref2.setSummary("Current amount is " + getPreferenceScreen().getSharedPreferences().getString(OPT_FAV_AMOUNT[1], OPT_FAV_AMOUNT_DEF[1]));
+		mEditTextPref3.setSummary("Current amount is " + getPreferenceScreen().getSharedPreferences().getString(OPT_FAV_AMOUNT[2], OPT_FAV_AMOUNT_DEF[2]));
+		mEditTextPref4.setSummary("Current amount is " + getPreferenceScreen().getSharedPreferences().getString(OPT_FAV_AMOUNT[3], OPT_FAV_AMOUNT_DEF[3]));
+		mEditTextPref5.setSummary("Current amount is " + getPreferenceScreen().getSharedPreferences().getString(OPT_FAV_AMOUNT[4], OPT_FAV_AMOUNT_DEF[4]));
+		
+		// Register listener for when a key changes
+		getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		
+		// Unregister listener for when a key changes
+		getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
 	}
 	
 	/** Get the current value of daily goal amount */
@@ -53,6 +94,24 @@ public class Settings extends PreferenceActivity {
 			return prefAmount;
 		} catch (NumberFormatException nfe) {
 			return DEFAULT_FAV_AMOUNT;
+		}
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+		
+		// Update the preference summaries for favorite amounts
+		if (key.equals(OPT_FAV_AMOUNT[0])) {
+			mEditTextPref1.setSummary("Current amount is " + sharedPreferences.getString(key, OPT_FAV_AMOUNT_DEF[0]));
+		} else if (key.equals(OPT_FAV_AMOUNT[1])) {
+			mEditTextPref2.setSummary("Current amount is " + sharedPreferences.getString(key, OPT_FAV_AMOUNT_DEF[1]));
+		} else if (key.equals(OPT_FAV_AMOUNT[2])) {
+			mEditTextPref3.setSummary("Current amount is " + sharedPreferences.getString(key, OPT_FAV_AMOUNT_DEF[2]));
+		} else if (key.equals(OPT_FAV_AMOUNT[3])) {
+			mEditTextPref4.setSummary("Current amount is " + sharedPreferences.getString(key, OPT_FAV_AMOUNT_DEF[3]));
+		} else if (key.equals(OPT_FAV_AMOUNT[4])) {
+			mEditTextPref5.setSummary("Current amount is " + sharedPreferences.getString(key, OPT_FAV_AMOUNT_DEF[4]));
 		}
 	}
 }

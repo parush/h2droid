b6187d07ad81fac600912ecf54f88dab5593dce0
fcalise@gmail.com <fcalise@gmail.com@e497a985-b2e5-56b5-e06b-30a1cf0c0d81>
2011-02-12 18:25:53 +0000

    display large units and proper unit system for list adapter

--- src/com/frankcalise/h2droid/EntryListAdapter.java
+++ src/com/frankcalise/h2droid/EntryListAdapter.java
@@ -14,10 +14,15 @@ public class EntryListAdapter extends BaseAdapter {
 	private List<Entry> mEntryList;
 	private final LayoutInflater mInflater;
 	private boolean mIsDetail;
+	private boolean mLargeUnits;
+	private int mUnitSystem;
 	
 	public EntryListAdapter(List<Entry> _entryList, Context _context, boolean _isDetail) {
 		this.mEntryList = _entryList;
 		this.mIsDetail = _isDetail;
+		
+		this.mUnitSystem = Settings.getUnitSystem(_context);
+		this.mLargeUnits = Settings.getLargeUnitsSetting(_context);
 		
 		mInflater = LayoutInflater.from(_context);
 	}
@@ -50,19 +55,34 @@ public class EntryListAdapter extends BaseAdapter {
 				convertView = mInflater.inflate(R.layout.entry_detail_list_item, parent, false);
 			}
 		}
-
+		
+		double originalAmount = e.getNonMetricAmount();
+		double displayAmount = e.getNonMetricAmount();
+		String displayUnits = "fl oz";
+		if (mUnitSystem == Settings.UNITS_METRIC) {
+			displayAmount /= Entry.ouncePerMililiter;
+			displayUnits = "mL";
+		}
+		
+		
+		if (mLargeUnits) {
+			Amount currentAmount = new Amount(originalAmount, mUnitSystem);
+    		displayAmount = currentAmount.getAmount();
+    		displayUnits = currentAmount.getUnits();
+		}
+		
 		if (mIsDetail == false) {
 			// Set date
 			((TextView)convertView.findViewById(R.id.entry_date_textview)).setText(e.getDateWithFormat("EEEE, MMMM dd, yyyy"));
 			
 			// Set amount
-			((TextView)convertView.findViewById(R.id.entry_amount_textview)).setText(String.format("  %.1f fl oz", e.getNonMetricAmount()));
+			((TextView)convertView.findViewById(R.id.entry_amount_textview)).setText(String.format("  %.1f %s", displayAmount, displayUnits));
 		} else {
 			// Set date
-			((TextView)convertView.findViewById(R.id.entry_date_textview)).setText(e.getDateWithFormat("hh:m:s a"));
+			((TextView)convertView.findViewById(R.id.entry_date_textview)).setText(e.getDateWithFormat("hh:mm:ss a"));
 			
 			// Set amount
-			((TextView)convertView.findViewById(R.id.entry_amount_textview)).setText(String.format("%.1f fl oz", e.getNonMetricAmount()));
+			((TextView)convertView.findViewById(R.id.entry_amount_textview)).setText(String.format("%.1f %s", displayAmount, displayUnits));
 		}
 		
 		return convertView;

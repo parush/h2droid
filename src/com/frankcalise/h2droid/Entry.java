88b8167647c9ef390ed497ac874e2a19012465e0
fcalise@gmail.com <fcalise@gmail.com@e497a985-b2e5-56b5-e06b-30a1cf0c0d81>
2011-03-20 04:13:13 +0000

    cleanup

--- src/com/frankcalise/h2droid/Entry.java
+++ src/com/frankcalise/h2droid/Entry.java
@@ -21,7 +21,6 @@ public class Entry {
 			sdf = new SimpleDateFormat(_format);
 			formattedDate = sdf.format(date);
 		} catch (ParseException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
